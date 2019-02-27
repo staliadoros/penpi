073f1393c1fee429851e45b30526bc9068db5bc2
pi <pi@calamari>
2019-02-26 22:05:13 -0800

    Adding every file for flask server

---
+++ imu_sample.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python
+#import matplotlib.pyplot as plt
+from mpu9250 import mpu9250
+from time import sleep
+from time import time
+
+
+def take_sample(sample_time, name):
+    ##Write to CSV
+    
+    imu = mpu9250(mpu_addr=0x69)
+    import csv
+     
+    #ifile  = open('test.csv', "rb")
+    #reader = csv.reader(ifile)
+    ofile  = open(name, "wb")
+    write  = csv.writer(ofile, delimiter=',', quotechar='"', quoting=csv.QUOTE_ALL)
+
+
+    ##Loop to execute IMU data read
+    starttime   =  time()*1000
+    output_data = []
+    while time()*1000 - starttime < sample_time * 1000:
+            try:
+                    a = imu.accel
+                    accelerometer_data = list(a)
+                    output_data.append(accelerometer_data)
+                    # print 'Accel: {:.3f} {:.3f} {:.3f} mg'.format(*a)
+                    # g = imu.gyro
+                    # print 'Gyro: {:.3f} {:.3f} {:.3f} dps'.format(*g)
+                    # m = imu.mag
+                    # print 'Magnet: {:.3f} {:.3f} {:.3f} mT'.format(*m)
+                    #m = imu.temp
+                    #print 'Temperature: {:.3f} C'.format(m * 9.0/5.0 + 32.)
+                    write.writerow(accelerometer_data)
+            except KeyboardInterrupt:
+                    print 'bye ...'
+    ofile.close()
+    return output_data
+
---
+++ pen_server.py
@@ -0,0 +1,10 @@
+from flask import Flask
+from imu_sample import take_sample
+app = Flask(__name__)
+
+@app.route('/')
+def hello_world():
+    return str(take_sample(10, 'server_sample.csv'))
+    
+
+app.run(host='0.0.0.0',port=5000)
