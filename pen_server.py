1794f17a06ab3aa6136edafaab482c23c970194b
Stephen Taliadoros <staliadoros@elon.edu>
2019-03-19 21:26:59 -0700

    Added new route to flask app

--- pen_server.py
+++ pen_server.py
@@ -5,6 +5,9 @@ app = Flask(__name__)
 @app.route('/')
 def hello_world():
     return str(take_sample(10, 'server_sample.csv'))
-    
+
+@app.route('/stef')
+def stef():
+    return 'Kyle is a betch'
 
 app.run(host='0.0.0.0',port=5000)
